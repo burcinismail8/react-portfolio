1d13c9d112730143fdfa39bccd794fdefc35fbee
Burchin Ismail <burcinismail8@gmail.com>
2023-05-14 12:56:57 +0300

    Finished styling

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,19 @@ import devBI from "../../public/AvatarMaker.png";
 import design from "../../public/design.png";
 import code from "../../public/code.png";
 import consulting from "../../public/consulting.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
+
 export default function Home() {
   return (
-    <main className="bg-white px-10">
+    <main className="bg-white px-10 md:px-20 lg:px-40">
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl font-burtons">developed by burchin</h1>
+          <h1 className="text-xl font-burtons">developedbyburchin</h1>
           <ul className="flex items-center">
             <li>
               <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
@@ -26,11 +33,11 @@ export default function Home() {
           </ul>
         </nav>
         <div className="text-center p-10">
-          <h2 className="text-5xl py-2 text-lightCherry font-medium">
+          <h2 className="text-5xl py-2 text-lightCherry font-medium md:text-6xl">
             Burchin Ismail
           </h2>
-          <h3 className="text-2xl py-2">Web Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800">
+          <h3 className="text-2xl py-2 md:text-3xl">Web Developer.</h3>
+          <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
             As a web developer with a passion for innovation and attention to
             detail, I specialize in building custom websites tailored to meet
             your unique needs. With proficiency in HTML, CSS, JavaScript, and
@@ -43,7 +50,7 @@ export default function Home() {
           <AiFillLinkedin />
           <AiFillInstagram />
         </div>
-        <div className="relative mt-20 mx-auto overflow-hidden bg-gradient-to-b from-lightCherry rounded-full w-80 h-80">
+        <div className="relative mt-20 mx-auto overflow-hidden bg-gradient-to-b from-lightCherry rounded-full w-80 h-80 md:h-96 md:w-96">
           <Image src={devBI} fill style={"cover"} />
         </div>
       </section>
@@ -133,6 +140,62 @@ export default function Home() {
             your goals. Let's work together to bring your website to the next
             level!
           </p>
+        </div>
+        <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web1}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              responsive
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web2}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              responsive
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web3}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              responsive
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web4}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              responsive
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web5}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              responsive
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web6}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              responsive
+            />
+          </div>
         </div>
       </section>
     </main>
