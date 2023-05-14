4d78d32e111a11a26130feb71e0b8a35aa1ae566
Burchin Ismail <burcinismail8@gmail.com>
2023-05-14 12:35:30 +0300

    Added new sections

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
 import Image from "next/image";
 import devBI from "../../public/AvatarMaker.png";
+import design from "../../public/design.png";
+import code from "../../public/code.png";
+import consulting from "../../public/consulting.png";
 export default function Home() {
   return (
     <main className="bg-white px-10">
@@ -47,6 +50,74 @@ export default function Home() {
       <section>
         <div>
           <h3 className="text-3xl py-1">Services I offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            As a front-end developer, I bring designs to life with responsive
+            and interactive user interfaces. With expertise in{" "}
+            <span className="text-midCherry">
+              HTML, CSS, JavaScript, React and Tailwind
+            </span>
+            , I build exceptional websites that deliver seamless user
+            experiences.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            Whether you're looking to build a new website or optimize an
+            existing one, I have the skills and expertise to help you achieve
+            your goals. Let's work together to bring your website to the next
+            level!
+          </p>
+        </div>
+        <div className="lg:flex gap-10">
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={design} width={100} height={100} className="mx-auto" />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs following core
+              design theory.
+            </p>
+            <h4 className="py-4 text-midCherry">Design tools I use</h4>
+            <p className="text-gray-800 py1">Photshop</p>
+            <p className="text-gray-800 py1">Illustrator</p>
+            <p className="text-gray-800 py1">Figma</p>
+            <p className="text-gray-800 py-1">Indesign</p>
+          </div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={code} width={100} height={100} className="mx-auto" />
+            <h3 className="text-lg font-medium pt-8 pb-2">
+              Code your dream project
+            </h3>
+            <p className="py-2">
+              Do you have an idea for your next great website? Let's make it a
+              reality.
+            </p>
+            <h4 className="py-4 text-midCherry">Coding tools I use</h4>
+            <p className="text-gray-800 py1">Visual Studio</p>
+            <p className="text-gray-800 py1">Visual Studio Code</p>
+            <p className="text-gray-800 py1">Eclipse</p>
+            <p className="text-gray-800 py-1">GitLab</p>
+          </div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image
+              src={consulting}
+              width={100}
+              height={100}
+              className="mx-auto"
+            />
+            <h3 className="text-lg font-medium pt-8 pb-2">Consulting</h3>
+            <p className="py-2">
+              Are you interested in feedback for your current project? I can
+              give you tips and tricks to level it up.
+            </p>
+            <h4 className="py-4 text-midCherry">Consulting tools I use</h4>
+            <p className="text-gray-800 py1">Jira</p>
+            <p className="text-gray-800 py1">Azure</p>
+            <p className="text-gray-800 py1">Skype</p>
+            <p className="text-gray-800 py-1">Microsoft Teams</p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3 className="text-3xl py-1">Portfolio</h3>
           <p className="text-md py-2 leading-8 text-gray-800">
             As a front-end developer, I bring designs to life with responsive
             and interactive user interfaces. With expertise in{" "}
